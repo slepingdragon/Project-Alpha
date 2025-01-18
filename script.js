4bdf4cdbce348012aac360dacbb050f25dc6860d
brady b <155825919+slepingdragon@users.noreply.github.com>
2025-01-18 14:41:00 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// let playername = prompt('whats your name? ');
+// console.log(('welcome to the game',playername+'!'))
+
+// get canvas element and its context
+let canvas = document.getElementById("gamecanvas");
+let ctx = canvas.getContext("2d")
+
+// draw a red rectangle
+ctx.fillStyle = 'darkgreen';
+ctx.fillRect(0, 0, 2000, 2000); // x , y , width, height
+
+let boolean = True
+
+if boolean == True {
+    while (boolean=True) {
+}
+
+
+}
